9ff575811c8016a19d868f0f57b5efc5944f0f46
Andrej Pregl <andrej.pregl@icloud.com>
2023-11-16 16:09:24 +0100

    Initial commit

---
+++ src/main/kotlin/com/epidemicsound/sounds/SoundsApplication.kt
@@ -0,0 +1,11 @@
+package com.epidemicsound.sounds
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoundsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoundsApplication>(*args)
+}
---
+++ src/test/kotlin/com/epidemicsound/sounds/SoundsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.epidemicsound.sounds
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoundsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
