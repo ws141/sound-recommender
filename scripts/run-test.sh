22d4bed6e7b4653429f62890a5538ab959f70843
Andrej Pregl <andrej.pregl@icloud.com>
2023-11-16 16:31:19 +0100

    Added script for running tests

---
+++ scripts/run-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -t postman/newman_alpine33 run https://api.postman.com/collections/31033780-840020f0-ae6c-4fbd-af7d-db82477080b6?access_key=PMAT-01HF9JAM02EGRYKKRPF0JJ03M2
