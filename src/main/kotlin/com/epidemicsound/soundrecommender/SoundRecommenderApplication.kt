73328f8a063146952d470b1b4dcfddd26af34d51
Andrej Pregl <andrej.pregl@icloud.com>
2023-11-20 22:57:48 +0100

    Rename application files

--- src/main/kotlin/com/epidemicsound/soundrecommender/SoundRecommenderApplication.kt
+++ src/main/kotlin/com/epidemicsound/soundrecommender/SoundRecommenderApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SoundsApplication
+class SoundRecommenderApplication
 
 fun main(args: Array<String>) {
-    runApplication<SoundsApplication>(*args)
+    runApplication<SoundRecommenderApplication>(*args)
 }
